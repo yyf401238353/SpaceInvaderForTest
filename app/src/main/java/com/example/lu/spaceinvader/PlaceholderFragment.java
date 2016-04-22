a17b009c273e0610bad6a5abdcd4d936b0c5b4a0
Yifan Yang <young_ivan@qq.com>
2016-04-22 15:29:51 +0800

    fragment 的使用

---
+++ app/src/main/java/com/example/lu/spaceinvader/PlaceholderFragment.java
@@ -0,0 +1,21 @@
+package com.example.lu.spaceinvader;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by lu on 2016-04-22.
+ */
+public class PlaceholderFragment extends Fragment {
+
+    public  PlaceholderFragment(){
+
+    }
+    public View onCreateView(LayoutInflater infalater,ViewGroup container,Bundle saveInstanceState){
+          View rootView=infalater.inflate(R.layout.fragment_main,container,false);
+        return rootView;
+    }
+}
